855bc2b722014de663230b5a25f8d8214ad966b2
oskari6 <oskarisulkakoski@gmail.com>
2024-04-10 18:17:49 +0300

    4/10/2024

---
+++ webhooks1.py
@@ -0,0 +1,18 @@
+import requests
+
+payloadurl = "discord api here, where you send everything" 
+
+while True:
+    message = input()
+    r = requests.post(url, data={"content":message})
+
+#online money transfer checking function
+def get_balance(address):
+    url = f"http//54.183.17.224/accounts/{address}/balance"
+    print("url:", url)
+    r = requests.get(url)
+    balance = r.json().get("balance")
+    if balance is None:
+        return 0
+    else:
+        return balance
